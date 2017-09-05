3fb8876e8917a6256fce1934394049d2f52c1c27
Alex Benoit <alex@lewagon.com>
2017-09-05 17:54:39 +0100

    init commit

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
 
+gem 'twilio-ruby'
+
 group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  # skip_before_action :authenticate_user!,  only: [:home]
 
   def home
+    require 'twilio-ruby'
+
+    @tutor = 'alex@lewagon.com'
+
+    identity = current_user.email
+    @token = Twilio::JWT::AccessToken.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_API_KEY'], ENV['TWILIO_API_SECRET'], identity: identity)
+
+    grant = Twilio::JWT::AccessToken::VideoGrant.new
+    # grant.room = 'DailyStandup'
+    @token.add_grant(grant)
   end
 end
+
+
